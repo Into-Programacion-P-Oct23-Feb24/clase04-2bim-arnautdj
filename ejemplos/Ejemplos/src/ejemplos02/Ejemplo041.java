4fc677f3e6e4bc6c70138a66b98b6e100c6a9d4d
arnautdj <sfrosales@utpl.edu.ec>
2024-01-04 00:05:35 -0500

    Ejercicio en clase

--- ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
@@ -14,25 +14,35 @@ public class Ejemplo041 {
     public static void main(String[] args) {
         // 
         int [][]primerValor = {{1,2,3},{3,2,4},{2,6,2}};
-        int [][]segundoValor = {{1,2,3},{3,2,2},{3,1,2}};
+        int [][]segundoValor = {{1,2,3},{2,2,2},{3,1,2}};
         int [][]arreglo3 = new int[3][3];
         
         for (int i = 0; i < primerValor.length; i++) {
             for (int j = 0; j < segundoValor.length; j++) {
-                System.out.printf("%d", primerValor[i][j]);
-            }  
+                System.out.printf("%d\t", primerValor[i][j]);
+            }
+            System.out.println("\n");
         }
         
-        
-    
+        System.out.println("\n");
         
         for (int i = 0; i < primerValor.length; i++) {
             for (int j = 0; j < segundoValor.length; j++) {
-                arreglo3[i][j] = obtenerMultiplicacion(primerValor[i][j], segundoValor[i][j]);
-            }  
+                System.out.printf("%d\t", segundoValor[i][j]);
+            }
+            System.out.println("\n");
         }
         
-        
+        System.out.println("\n");
+
+        for (int i = 0; i < primerValor.length; i++) {
+            for (int j = 0; j < segundoValor.length; j++) {
+                arreglo3[i][j] = obtenerMultiplicacion(primerValor[i][j], segundoValor[i][j]);
+                System.out.printf("%d\t", arreglo3[i][j]);
+            }  
+            System.out.println("\n");
+        }
+  
     }
         
     public static int obtenerMultiplicacion(int a, int b){
